8f4a7ea60d10696feba69607be1ab84e61a36b0f
John Paul Liberal <jliberal@gmail.com>
2020-09-09 15:10:15 -0300

    HomePage UI done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App () {
-  return (
-    <div className="app">
-      <Header />
-    </div>
-  );
+function App() {
+	return (
+		<Router>
+			<div className="app">
+				<Switch>
+					<Route path="/checkout">
+						<Header />
+						<h1> I'm the checkout page </h1>
+					</Route>
+					<Route path="/">
+						<Header />
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import '../styles/Home.css';
+import Product from './Product';
+
+function Home() {
+	return (
+		<div className="home">
+			<div className="home__container">
+				<img
+					className="home__image"
+					src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+					alt=""
+				/>
+
+				<div className="home__row">
+					<Product
+						title="The Lean Startup"
+						price={29.99}
+						image="https://m.media-amazon.com/images/I/81-QB7nDh4L._AC_UY218_.jpg"
+						rating={5}
+					/>
+					<Product
+						title="The Lean Startup"
+						price={29.99}
+						image="https://m.media-amazon.com/images/I/81-QB7nDh4L._AC_UY218_.jpg"
+						rating={5}
+					/>
+				</div>
+				<div className="home__row">
+					<Product
+						title="The Lean Startup"
+						price={29.99}
+						image="https://m.media-amazon.com/images/I/81-QB7nDh4L._AC_UY218_.jpg"
+						rating={5}
+					/>
+					<Product
+						title="The Lean Startup"
+						price={29.99}
+						image="https://m.media-amazon.com/images/I/81-QB7nDh4L._AC_UY218_.jpg"
+						rating={5}
+					/>
+				</div>
+				<div className="home__row">
+					<Product
+						title="The Lean Startup"
+						price={29.99}
+						image="https://m.media-amazon.com/images/I/81-QB7nDh4L._AC_UY218_.jpg"
+						rating={5}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Home;
